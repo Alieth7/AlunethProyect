a2d0e403bdee8f7013f3c1bd40fa56625ffa3ce8
Sega1A <sergio.garciacuchallo@gmail.com>
2024-10-08 19:36:10 -0400

    SQL Query changed, added Readme to Server folder.

---
+++ server/README.md
@@ -0,0 +1,48 @@
+# API de Entregas y Pedidos(Venta) de Cuadernos Aluneth
+
+Este repositorio tiene como objetivo realizar una API REST hecha en ExpressJS
+
+### Instalacion
+
+1. Clonar repositorio
+
+```sh
+https://github.com/Alieth7/AlunethProyect.git
+```
+
+2. Ingresar a la carpeta **Server**
+
+```sh
+cd ./server
+```
+
+3. Instalar los paquetes NPM
+
+```sh
+npm install
+```
+
+4. Configurar archivo .env
+
+```sh
+PORT=3000 #Server port
+HOST=localhost
+
+# MYSQLServer Database
+DB_USER=your_user
+DB_PASSWORD=your_password
+DB_DATABASE=your_database
+DB_PORT=3306
+```
+
+5. Correr SQL en MysqlServer de la ruta.
+
+```sh
+../server/src/db/queries.sql
+```
+
+6. Iniciar servidor
+
+```sh
+npm run start
+```
--- server/src/db/queries.sql
+++ server/src/db/queries.sql
@@ -3,10 +3,58 @@ CREATE DATABASE IF NOT EXISTS AlunethHandmade;
 
 USE AlunethHandmade;
 
-CREATE TABLE IF NOT EXISTS product (
-    id INT(11) NOT NULL AUTO_INCREMENT,
-    name VARCHAR(45) NOT NULL,
-    description VARCHAR(100) NOT NULL,
-    price DECIMAL(10, 2) NOT NULL,
+CREATE TABLE IF NOT EXISTS Persona (
+	id INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(30) NOT NULL,
+    ci INT NOT NULL,
+    apellido VARCHAR(30) NOT NULL,
+    nro_celular INT NOT NULL,
+    rol VARCHAR(30),
     PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS Cuenta (
+    id INT NOT NULL AUTO_INCREMENT,
+    nro_cuenta INT NOT NULL,
+    nombre_usuario VARCHAR(30) NOT NULL,
+    contrasenia VARCHAR(35) NOT NULL,
+    id_persona INT NULL,
+    FOREIGN KEY (id_persona) REFERENCES Persona(id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS Pedido (
+	id INT NOT NULL AUTO_INCREMENT,
+    numero_pedido INT NOT NULL,
+    fecha_pedido DATE NOT NULL,
+    fecha_recojo DATE NOT NULL,
+    estado VARCHAR(30) NOT NULL,
+    ci_destinatario INT NOT NULL,
+    nombre_destinatario VARCHAR(30),
+    total INT NOT NULL,
+    id_persona INT NOT NULL,
+    id_cuenta INT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_persona) REFERENCES Persona(id),
+    FOREIGN KEY (id_cuenta) REFERENCES Cuenta(id)
+);
+
+CREATE TABLE IF NOT EXISTS Cuaderno (
+	id INT NOT NULL AUTO_INCREMENT,
+    codigo_de_catalogo VARCHAR(10) NOT NULL,
+    precio FLOAT NOT NULL,
+    personalizado BOOLEAN NOT NULL,
+    nombre VARCHAR(30) NOT NULL,
+    especificaciones VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS Pedido_Cuaderno (
+	id INT NOT NULL AUTO_INCREMENT,
+    id_pedido INT NOT NULL,
+    id_cuaderno INT NOT NULL,
+    cantidad INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_pedido) REFERENCES Pedido(id),
+    FOREIGN KEY (id_cuaderno) REFERENCES Cuaderno(id)
 );
